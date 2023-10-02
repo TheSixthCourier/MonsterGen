25b6ee13f79083e65b55b34965a7042f117db080
TheSixthCourier <146359602+TheSixthCourier@users.noreply.github.com>
2023-10-02 10:11:39 -0600

    Update MonsterGen.cpp

--- MonsterGen.cpp
+++ MonsterGen.cpp
@@ -18,9 +18,9 @@ void MonsterGen() {
 	int num = rand() % 100 + 1; //Creates a number between 1-100
 	if (num < 15) //15% chance.
 		cout << "A Witch stands before you. She has a small stand set up with a couple of potions and poisons on display." << endl;
-	else if (num < 20) //5% chance, I feel like I'm gonna need to smoke something just to understand this math.
+	else if (num < 20) //5% chance.
 1		cout << "A Young Werewolf is crouched ahead of you, it's trying to stay out of your line of sight." << endl;
-	else if (num < 50) //30%
+	else if (num < 50) //30% chance.
 		cout << "A Large Humanoidic Dragoness lies before you,\n She's fast asleep and clutching a skeleton who doesn't seem to like it one bit." << endl;
 	else if (num < 75) //50% chance.
 		cout << "A Skeleton is sitting down in front of you, he's wearing some rusted chainmail and a rotting tabbard, Behind him is a Large Humanoidic Dragnoness." << endl;
